d3db494d26ecf96a58d0a6c0d092e2366a1205bd
jimatas <atask@hr.nl>
2022-05-02 10:23:39 +0200

    Doc-commented method.

--- HR.BrightspaceConnector/Utilities/JsonTypeExtensions.cs
+++ HR.BrightspaceConnector/Utilities/JsonTypeExtensions.cs
@@ -6,6 +6,14 @@ namespace HR.BrightspaceConnector.Utilities
 {
     internal static class JsonTypeExtensions
     {
+        /// <summary>
+        /// Returns the JSON name of a property, which is either the name that is configured through a possible <see cref="JsonPropertyNameAttribute"/>, or 
+        /// if not present, the name returned by the optionally specified <see cref="JsonNamingPolicy"/>, or failing that, the supplied property name as is.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="propertyName"></param>
+        /// <param name="propertyNamingPolicy"></param>
+        /// <returns></returns>
         public static string GetJsonPropertyName(this Type target, string propertyName, JsonNamingPolicy? propertyNamingPolicy = null)
         {
             var property = target.GetProperty(propertyName);
