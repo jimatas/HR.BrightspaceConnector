e9ceb704ec25af7ce3a489cc11c78d4408c41ce7
jimatas <atask@hr.nl>
2022-05-10 15:59:10 +0200

    Application hosted as Windows service.

--- HR.BrightspaceConnector.Worker/Program.cs
+++ HR.BrightspaceConnector.Worker/Program.cs
@@ -4,5 +4,6 @@ using HR.Common.Utilities;
 await Host.CreateDefaultBuilder(args)
     .UseStartup<Startup>()
     .UseDefaultServiceProvider(options => options.ValidateScopes = false)
+    .UseWindowsService()
     .Build()
     .RunAsync().WithoutCapturingContext();
