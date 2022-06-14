7f42cfb925d4c807c83e24e358d855a1dc86fc8c
jimatas <atask@hr.nl>
2022-06-14 13:24:46 +0200

    Use the correct sprocs in Database depending on deployment environment.

---
+++ HR.BrightspaceConnector.Worker/Infrastructure/Hosting/HostEnvironmentExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.BrightspaceConnector.Infrastructure.Hosting
+{
+    internal static class HostEnvironmentExtensions
+    {
+        /// <summary>
+        /// Gets the appropriate value for that part of a stored procedure's name that is used to indicate its intended deployment environment.
+        /// For example, the string "prod" in the stored procedure name "sync_out_brightspace_prod_user_GetNextEvents".
+        /// </summary>
+        /// <example>test, prod</example>
+        /// <param name="environment"></param>
+        /// <returns></returns>
+        public static string GetStoredProcedureEnvironmentName(this IHostEnvironment environment)
+        {
+            return environment.IsProduction() ? "prod" : "test";
+        }
+    }
+}
--- HR.BrightspaceConnector.Worker/Infrastructure/Persistence/Database.cs
+++ HR.BrightspaceConnector.Worker/Infrastructure/Persistence/Database.cs
@@ -1,4 +1,5 @@
 ﻿using HR.BrightspaceConnector.Features.Users;
+using HR.BrightspaceConnector.Infrastructure.Hosting;
 using HR.Common.Utilities;
 
 using Microsoft.EntityFrameworkCore;
@@ -8,16 +9,34 @@ namespace HR.BrightspaceConnector.Infrastructure.Persistence
     public class Database : IDatabase
     {
         private readonly BrightspaceDbContext dbContext;
-        public Database(BrightspaceDbContext dbContext) => this.dbContext = dbContext;
+        private readonly IHostEnvironment environment;
+        private readonly ILogger logger;
+
+        public Database(BrightspaceDbContext dbContext, IHostEnvironment environment, ILogger<Database> logger)
+        {
+            this.dbContext = dbContext;
+            this.environment = environment;
+            this.logger = logger;
+        }
 
         public async Task<UserRecord?> GetNextUserAsync(CancellationToken cancellationToken = default)
         {
-            var users = await dbContext.Users.FromSqlRaw("sync_brightspace_prod_user_GetNextEvents").AsNoTracking().ToListAsync(cancellationToken).WithoutCapturingContext();
+            var sprocName = string.Format("sync_out_brightspace_{0}_user_GetNextEvents", environment.GetStoredProcedureEnvironmentName());
+            logger.LogDebug("Executing stored procedure '{SprocName}'.", sprocName);
+
+            var users = await dbContext.Users.FromSqlRaw(sprocName).AsNoTracking().ToListAsync(cancellationToken).WithoutCapturingContext();
             return users.SingleOrDefault();
         }
 
-        public async Task MarkAsHandledAsync(int eventId, bool success, int? id, string? message, CancellationToken cancellationToken = default)
+        public async Task MarkAsHandledAsync(
+            int eventId,
+            bool success,
+            int? id,
+            string? message,
+            CancellationToken cancellationToken = default)
         {
+            logger.LogDebug("Executing stored procedure 'sync_event_MarkHandled'.");
+
             await dbContext.Database.ExecuteSqlInterpolatedAsync($"sync_event_MarkHandled {eventId}, {success}, {id?.ToString()}, {message}", cancellationToken).WithoutCapturingContext();
         }
     }
