028393d10b77d98338e5f5e7eb3138d8863e9666
jimatas <atask@hr.nl>
2022-06-15 10:32:29 +0200

    Some more tests added.

--- HR.BrightspaceConnector.Tests/UsersTests.cs
+++ HR.BrightspaceConnector.Tests/UsersTests.cs
@@ -1,8 +1,10 @@
 ﻿using HR.BrightspaceConnector.Features.Users;
+using HR.BrightspaceConnector.Infrastructure;
 using HR.Common.Utilities;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,7 +22,83 @@ namespace HR.BrightspaceConnector.Tests
             Assert.IsTrue(roles.Any());
 
             roles = roles.Where(role => role.DisplayName == "Learner");
-            Assert.IsTrue(roles.Any());
+            Assert.IsTrue(roles.Any(), "roles.Any()");
+        }
+
+        [TestMethod]
+        public async Task GetUsersAsync_WithNoParameters_ReturnsPagedResultSet()
+        {
+            IApiClient apiClient = CreateApiClient();
+
+            var queryParams = new UserQueryParameters();
+
+            var users = await apiClient.GetUsersAsync(queryParams).WithoutCapturingContext();
+            Assert.IsInstanceOfType(users, typeof(IEnumerable<UserData>));
+            Assert.IsInstanceOfType(users, typeof(PagedResultSet<UserData>));
+            Assert.IsTrue(users.Any(), "users.Any()");
+        }
+
+        [TestMethod]
+        public async Task GetUsersAsync_GivenBookmarkParameter_ReturnsPagedResultSet()
+        {
+            IApiClient apiClient = CreateApiClient();
+
+            var queryParams = new UserQueryParameters
+            {
+                Bookmark = "invalid-bookmark"
+            };
+
+            var users = await apiClient.GetUsersAsync(queryParams).WithoutCapturingContext();
+            Assert.IsInstanceOfType(users, typeof(IEnumerable<UserData>));
+            Assert.IsInstanceOfType(users, typeof(PagedResultSet<UserData>));
+        }
+
+        [TestMethod]
+        public async Task GetUsersAsync_GivenOrgDefinedIdParameter_ReturnsEnumerable()
+        {
+            IApiClient apiClient = CreateApiClient();
+
+            var queryParams = new UserQueryParameters
+            {
+                OrgDefinedId = "ja.hstest@hro.nl"
+            };
+
+            var users = await apiClient.GetUsersAsync(queryParams).WithoutCapturingContext();
+            Assert.IsInstanceOfType(users, typeof(IEnumerable<UserData>));
+            Assert.IsNotInstanceOfType(users, typeof(PagedResultSet<UserData>));
+            Assert.IsFalse(users.Any(), "users.Any()");
+        }
+
+        [TestMethod]
+        public async Task GetUsersAsync_GivenExternalEmailParameter_ReturnsEnumerable()
+        {
+            IApiClient apiClient = CreateApiClient();
+
+            var queryParams = new UserQueryParameters
+            {
+                ExternalEmail = "ja.hstest@hr.nl"
+            };
+
+            var users = await apiClient.GetUsersAsync(queryParams).WithoutCapturingContext();
+            Assert.IsInstanceOfType(users, typeof(IEnumerable<UserData>));
+            Assert.IsNotInstanceOfType(users, typeof(PagedResultSet<UserData>));
+            Assert.IsFalse(users.Any(), "users.Any()");
+        }
+
+        [TestMethod]
+        public async Task GetUsersAsync_GivenUserNameParameter_ReturnsEnumerable()
+        {
+            IApiClient apiClient = CreateApiClient();
+
+            var queryParams = new UserQueryParameters
+            {
+                UserName = "ja.hstest"
+            };
+
+            var users = await apiClient.GetUsersAsync(queryParams).WithoutCapturingContext();
+            Assert.IsInstanceOfType(users, typeof(IEnumerable<UserData>));
+            Assert.IsNotInstanceOfType(users, typeof(PagedResultSet<UserData>));
+            Assert.IsFalse(users.Any(), "users.Any()");
         }
 
         [TestMethod]
