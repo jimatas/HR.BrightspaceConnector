65b412a41e14d01ab7f54d48673bcbc9ea132488
jimatas <atask@hr.nl>
2022-06-21 08:22:38 +0200

    Recovery tests.

---
+++ HR.BrightspaceConnector.Tests/Fixture/SqlTimeoutException.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Common;
+
+namespace HR.BrightspaceConnector.Tests.Fixture
+{
+    public class SqlTimeoutException : DbException
+    {
+        public const string DefaultMessage = "Execution Timeout Expired. The timeout period elapsed prior to completion of the operation or the server is not responding.";
+
+        public SqlTimeoutException() : this(DefaultMessage) { }
+        public SqlTimeoutException(string customMessage) : base(customMessage) { }
+    }
+}
---
+++ HR.BrightspaceConnector.Tests/RecoveryTests.cs
@@ -0,0 +1,67 @@
+﻿using HR.BrightspaceConnector.Features.Users;
+using HR.BrightspaceConnector.Features.Users.Queries;
+using HR.BrightspaceConnector.Infrastructure.Persistence;
+using HR.BrightspaceConnector.Tests.Fixture;
+using HR.Common.Cqrs.Queries;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Moq;
+
+using System;
+using System.Data.Common;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HR.BrightspaceConnector.Tests
+{
+    [TestClass]
+    public class RecoveryTests : DependencyInjectedTestsBase
+    {
+        private readonly Mock<IDatabase> mockedDatabase = new();
+        private readonly Mock<IApiClient> mockedApiClient = new();
+
+        [TestMethod]
+        public async Task GetNextUserQueryFailingOnFirstAttempt_WillBeRetried()
+        {
+            // Arrange
+            int attempt = 0;
+            mockedDatabase.Setup(database => database.GetNextUserAsync(default)).ReturnsAsync(new UserRecord
+            {
+                UserName = "ja.hstest",
+                OrgDefinedId = "ja.hstest@hro.nl",
+                RoleId = 117,
+                FirstName = "Jim",
+                LastName = "Atas",
+                SortLastName = "Atas",
+                ExternalEmail = "ja.hstest@hr.nl",
+                IsActive = true,
+                SendCreationEmail = false,
+                SyncEventId = Random.Shared.Next(1, int.MaxValue),
+                SyncAction = 'c',
+                SyncInternalKey = "ja.hstest"
+            }).Callback((CancellationToken _) =>
+            {
+                if (++attempt == 1)
+                {
+                    throw new SqlTimeoutException();
+                }
+            });
+
+            IServiceProvider serviceProvider = CreateServiceProvider(mockedDatabase.Object, mockedApiClient.Object);
+            IQueryDispatcher queryDispatcher = serviceProvider.GetRequiredService<IQueryDispatcher>();
+
+            // Act / Assert
+            try
+            {
+                var user = await queryDispatcher.DispatchAsync(new GetNextUser());
+                Assert.IsNotNull(user);
+            }
+            catch (DbException)
+            {
+                Assert.Fail("The exception should have been caught and the query retried.");
+            }
+        }
+    }
+}
