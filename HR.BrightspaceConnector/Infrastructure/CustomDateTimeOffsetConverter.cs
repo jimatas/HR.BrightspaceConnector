92eb01e667782b420e9a328f33a4dc95a2cfd62e
jimatas <atask@hr.nl>
2022-07-08 09:12:33 +0200

    Simplified custom DateTimeOffset JSON converter and ensured date/time is serialized to UTC.

--- HR.BrightspaceConnector/Infrastructure/CustomDateTimeOffsetConverter.cs
+++ HR.BrightspaceConnector/Infrastructure/CustomDateTimeOffsetConverter.cs
@@ -4,33 +4,23 @@ using System.Text.Json.Serialization;
 
 namespace HR.BrightspaceConnector.Infrastructure
 {
-    internal class CustomDateTimeOffsetConverter : JsonConverter<DateTimeOffset?>
+    internal class CustomDateTimeOffsetConverter : JsonConverter<DateTimeOffset>
     {
-        private const string DateTimeFormat = "yyyy-MM-ddTHH:mm:ss.fff'Z'";
+        private const string DateTimeFormat = "yyyy-MM-ddTHH:mm:ss.fffZ";
 
-        public override DateTimeOffset? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        public override DateTimeOffset Read(ref Utf8JsonReader jsonReader, Type typeToConvert, JsonSerializerOptions jsonOptions)
         {
-            return reader.GetString() is string value
-                && DateTimeOffset.TryParseExact(
-                    value,
-                    DateTimeFormat,
-                    CultureInfo.InvariantCulture,
-                    DateTimeStyles.None,
-                    out DateTimeOffset result)
-                ? result
-                : null;
+            return DateTimeOffset.TryParseExact(
+                jsonReader.GetString(),
+                DateTimeFormat,
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.None,
+                out DateTimeOffset value) ? value : throw new JsonException($"The JSON value could not be converted to {typeof(DateTimeOffset)}.");
         }
 
-        public override void Write(Utf8JsonWriter writer, DateTimeOffset? value, JsonSerializerOptions options)
+        public override void Write(Utf8JsonWriter jsonWriter, DateTimeOffset value, JsonSerializerOptions jsonOptions)
         {
-            if (value is null)
-            {
-                writer.WriteNullValue();
-            }
-            else
-            {
-                writer.WriteStringValue(((DateTimeOffset)value).ToString(DateTimeFormat, CultureInfo.InvariantCulture));
-            }
+            jsonWriter.WriteStringValue(value.ToUniversalTime().ToString(DateTimeFormat, CultureInfo.InvariantCulture));
         }
     }
 }
