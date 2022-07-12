e57dccefafb2e4cb855e1ecd6ef7f295da7554f4
jimatas <atask@hr.nl>
2022-07-12 10:37:44 +0200

    Initial ProcessCourseOffering unit test.

---
+++ HR.BrightspaceConnector.Tests/ProcessCourseOfferingsTests.cs
@@ -0,0 +1,42 @@
+﻿using HR.BrightspaceConnector.Features.Courses;
+using HR.BrightspaceConnector.Features.Courses.Commands;
+using HR.BrightspaceConnector.Infrastructure.Persistence;
+using HR.Common.Cqrs.Commands;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Moq;
+
+using System;
+using System.Threading.Tasks;
+
+namespace HR.BrightspaceConnector.Tests
+{
+    [TestClass]
+    public class ProcessCourseOfferingsTests : DependencyInjectedTestsBase
+    {
+        private readonly Mock<IDatabase> mockedDatabase = new();
+        private readonly Mock<IApiClient> mockedApiClient = new();
+
+        [DataTestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public async Task GivenNullCourseOfferingInAnyContext_DoesNothing(bool isDeleteContext)
+        {
+            // Arrange
+            mockedDatabase.Setup(database => database.GetNextCourseOfferingAsync(default)).ReturnsAsync(null as CourseOfferingRecord);
+
+            IServiceProvider serviceProvider = CreateServiceProvider(mockedDatabase.Object, mockedApiClient.Object);
+            ICommandDispatcher commandDispatcher = serviceProvider.GetRequiredService<ICommandDispatcher>();
+
+            // Act
+            await commandDispatcher.DispatchAsync(new ProcessCourseOfferings(batchSize: 1, isDeleteContext));
+
+            // Assert
+            mockedDatabase.Verify(database => database.GetNextCourseOfferingAsync(default), Times.Once());
+            mockedDatabase.VerifyNoOtherCalls();
+            mockedApiClient.VerifyNoOtherCalls();
+        }
+    }
+}
