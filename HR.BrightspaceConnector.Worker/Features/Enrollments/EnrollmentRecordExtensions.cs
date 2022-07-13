34726f23fe2058fcc4fdf39aac18621d17dbf5dd
jimatas <atask@hr.nl>
2022-07-13 08:42:15 +0200

    EnrollmentRecord extension methods.

---
+++ HR.BrightspaceConnector.Worker/Features/Enrollments/EnrollmentRecordExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace HR.BrightspaceConnector.Features.Enrollments
+{
+    public static class EnrollmentRecordExtensions
+    {
+        public static EnrollmentData ToEnrollmentData(this EnrollmentRecord enrollmentRecord)
+        {
+            return new EnrollmentData
+            {
+                IsCascading = null,
+                OrgUnitId = enrollmentRecord.OrgUnitId,
+                RoleId = enrollmentRecord.RoleId,
+                UserId = enrollmentRecord.UserId
+            };
+        }
+
+        public static CreateEnrollmentData ToCreateEnrollmentData(this EnrollmentRecord enrollmentRecord)
+        {
+            return new CreateEnrollmentData
+            {
+                OrgUnitId = enrollmentRecord.OrgUnitId,
+                RoleId = enrollmentRecord.RoleId,
+                UserId = enrollmentRecord.UserId
+            };
+        }
+    }
+}
