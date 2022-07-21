50f8b6facc03ecb545cf7afb9a73de589771607e
jimatas <atask@hr.nl>
2022-07-21 12:29:18 +0200

    Section record extension methods

---
+++ HR.BrightspaceConnector.Worker/Features/Sections/SectionRecordExtensions.cs
@@ -0,0 +1,19 @@
+﻿using HR.BrightspaceConnector.Features.Common;
+
+namespace HR.BrightspaceConnector.Features.Sections
+{
+    public static class SectionRecordExtensions
+    {
+        public static CreateOrUpdateSectionData ToCreateOrUpdateSectionData(this SectionRecord sectionRecord)
+        {
+            return new CreateOrUpdateSectionData
+            {
+                Code = sectionRecord.Code,
+                Description = !string.IsNullOrEmpty(sectionRecord.Description)
+                    ? new RichTextInput { Content = sectionRecord.Description, Type = TextContentType.Html }
+                    : null,
+                Name = sectionRecord.Name
+            };
+        }
+    }
+}
