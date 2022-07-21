f20be05cb86c6e249e33efc7321e0295b043d1b7
jimatas <atask@hr.nl>
2022-07-21 12:55:45 +0200

    Next section query and record extensions unit tests.

---
+++ HR.BrightspaceConnector.Tests/SectionRecordExtensionsTests.cs
@@ -0,0 +1,63 @@
+﻿using HR.BrightspaceConnector.Features.Sections;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+
+namespace HR.BrightspaceConnector.Tests
+{
+    [TestClass]
+    public class SectionRecordExtensionsTests
+    {
+        [TestMethod]
+        public void ToCreateOrUpdateSectionData_ByDefault_CopiesAllProperties()
+        {
+            // Arrange
+            var sectionRecord = new SectionRecord
+            {
+                Code = "Section-1",
+                Name = "Section 1",
+                Description = "This is the first section in the course offering.",
+                OrgUnitId = Random.Shared.Next(1, int.MaxValue),
+                SyncAction = 'c',
+                SyncEventId = Random.Shared.Next(1, int.MaxValue),
+                SyncExternalKey = null,
+                SyncInternalKey = Random.Shared.Next(1, int.MaxValue).ToString()
+            };
+
+            // Act
+            var sectionCreateData = sectionRecord.ToCreateOrUpdateSectionData();
+
+            // Assert
+            Assert.IsNotNull(sectionCreateData);
+            Assert.AreEqual(sectionRecord.Code, sectionCreateData.Code);
+            Assert.AreEqual(sectionRecord.Description, sectionCreateData.Description?.Content);
+            Assert.AreEqual(sectionRecord.Name, sectionCreateData.Name);
+        }
+
+        [DataTestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        public void ToCreateOrUpdateSectionData_GivenSectionRecordWithoutDescription_LeavesItOut(string? emptyValue)
+        {
+            // Arrange
+            var sectionRecord = new SectionRecord
+            {
+                Code = "Section-1",
+                Name = "Section 1",
+                Description = emptyValue,
+                OrgUnitId = Random.Shared.Next(1, int.MaxValue),
+                SyncAction = 'c',
+                SyncEventId = Random.Shared.Next(1, int.MaxValue),
+                SyncExternalKey = null,
+                SyncInternalKey = Random.Shared.Next(1, int.MaxValue).ToString()
+            };
+
+            // Act
+            var sectionCreateData = sectionRecord.ToCreateOrUpdateSectionData();
+
+            // Assert
+            Assert.IsNull(sectionCreateData.Description);
+        }
+    }
+}
---
+++ HR.BrightspaceConnector.Worker/Features/Sections/Queries/GetNextSection.cs
@@ -0,0 +1,33 @@
+﻿using HR.BrightspaceConnector.Infrastructure.Persistence;
+using HR.Common.Cqrs.Queries;
+using HR.Common.Utilities;
+
+namespace HR.BrightspaceConnector.Features.Sections.Queries
+{
+    public class GetNextSection : IQuery<SectionRecord?>
+    {
+    }
+
+    public class GetNextSectionHandler : IQueryHandler<GetNextSection, SectionRecord?>
+    {
+        private readonly IDatabase database;
+        private readonly ILogger logger;
+
+        public GetNextSectionHandler(IDatabase database, ILogger<GetNextSection> logger)
+        {
+            this.database = database;
+            this.logger = logger;
+        }
+
+        public async Task<SectionRecord?> HandleAsync(GetNextSection query, CancellationToken cancellationToken)
+        {
+            SectionRecord? section = await database.GetNextCourseOfferingSectionAsync(cancellationToken).WithoutCapturingContext();
+            if (section is not null)
+            {
+                logger.LogInformation("Retrieved section with code \"{SectionCode}\" for sync action '{SyncAction}' from database.", section.Code, section.SyncAction);
+            }
+
+            return section;
+        }
+    }
+}
