dd2d3cff39d2c3e72bb759cfecb9c3b5ea72702c
jimatas <atask@hr.nl>
2022-07-22 11:19:21 +0200

    Added unit test class for ProcessSections.

---
+++ HR.BrightspaceConnector.Tests/ProcessSectionsTests.cs
@@ -0,0 +1,42 @@
+﻿using HR.BrightspaceConnector.Features.Sections;
+using HR.BrightspaceConnector.Features.Sections.Commands;
+using HR.BrightspaceConnector.Infrastructure.Persistence;
+using HR.Common.Cqrs.Commands;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Moq;
+
+using System;
+using System.Threading.Tasks;
+
+namespace HR.BrightspaceConnector.Tests
+{
+    [TestClass]
+    public class ProcessSectionsTests : DependencyInjectedTestsBase
+    {
+        private readonly Mock<IDatabase> mockedDatabase = new();
+        private readonly Mock<IApiClient> mockedApiClient = new();
+
+        [DataTestMethod]
+        [DataRow(false)]
+        [DataRow(true)]
+        public async Task GivenNullSectionInAnyContext_DoesNothing(bool isDeleteContext)
+        {
+            // Arrange
+            mockedDatabase.Setup(database => database.GetNextCourseOfferingSectionAsync(default)).ReturnsAsync(null as SectionRecord);
+
+            IServiceProvider serviceProvider = CreateServiceProvider(mockedDatabase.Object, mockedApiClient.Object);
+            ICommandDispatcher commandDispatcher = serviceProvider.GetRequiredService<ICommandDispatcher>();
+
+            // Act
+            await commandDispatcher.DispatchAsync(new ProcessSections(batchSize: 1, isDeleteContext));
+
+            // Assert
+            mockedDatabase.Verify(database => database.GetNextCourseOfferingSectionAsync(default), Times.Once());
+            mockedDatabase.VerifyNoOtherCalls();
+            mockedApiClient.VerifyNoOtherCalls();
+        }
+    }
+}
